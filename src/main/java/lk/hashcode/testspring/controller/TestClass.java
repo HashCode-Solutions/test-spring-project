4c5decfac66f1fd4dd668fa49248933e363fe36e
Buddalaz <sandunsampath289@gmail.com>
2021-10-08 20:04:29 +0530

    initial commit

---
+++ src/main/java/lk/hashcode/testspring/controller/TestClass.java
@@ -0,0 +1,9 @@
+package lk.hashcode.testspring.controller;
+
+public class TestClass {
+
+    public void testMethod(){
+        System.out.println("test");
+    }
+
+}
---
+++ src/test/java/lk/hashcode/testspring/TestSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.hashcode.testspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
